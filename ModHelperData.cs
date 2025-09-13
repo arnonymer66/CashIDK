e8a07b53b5052cd9b5c397838120cfb1b0a04903
arnonymer66 <arnonymer649@gmail.com>
2025-09-13 18:33:52 +0200

    Update ModHelperData.cs

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -6,8 +6,6 @@ public static class ModHelperData
     public const string Version = "1.0.1";
     public const string Name = "CashIDK";
 
-    public const string Description = "Allows you to Multiply the Cash gained from ANY Source<br><br>";
-
     public const string RepoOwner = "arnonymer66"; 
     public const string RepoName = "CashIDK";
 }
