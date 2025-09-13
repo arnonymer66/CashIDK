13a44d48c32c1de7c8438db7bcee9293e35dcf6b
arnonymer66 <arnonymer649@gmail.com>
2025-09-13 19:29:15 +0200

    Create CashSource.cs

---
+++ CashSource.cs
@@ -0,0 +1,11 @@
+namespace Il2CppAssets.Scripts.Simulation
+{
+    public enum CashSource
+    {
+        None = 0,
+        BloonPop = 1,
+        Ability = 2,
+        Mod = 3,
+        Other = 4
+    }
+}
