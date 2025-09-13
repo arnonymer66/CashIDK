62298062bf8c5cd8ecd8d5b79dec8ae0dfc7dd9d
arnonymer66 <arnonymer649@gmail.com>
2025-09-13 22:04:30 +0200

    Update ModHelperData.cs

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -6,6 +6,7 @@ public static class ModHelperData
     public const string Version = "1.0.7";
     public const string Name = "CashIDK";
 
+    public const string Description = "Press (E) to Open The menu, than type any amount of money you want in game, than press Enter to Reedem it. Works only in game cash Not Monkey Dollar";
     public const string RepoOwner = "arnonymer66"; 
     public const string RepoName = "CashIDK";
 }
