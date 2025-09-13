30618c301a197815130bed1222a79848db1dcde4
arnonymer66 <arnonymer649@gmail.com>
2025-09-13 16:52:20 +0200

    Update ModHelperData.cs

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -1,10 +1,11 @@
-namespace CashIDK;
-
-public static class ModHelperData
+namespace CashIDK
 {
-    public const string Name = "CashIDK";
-    public const string Description = "Mod mit Geld-Multiplikator, One-Hit-Kill, Godmode etc.";
-    public const string Version = "1.0.1";
-    public const string RepoOwner = "arnonymer66"; // <--- Hier kannst du deinen Namen eintragen
-    public const string RepoName = "CashIDK";
+    public static class ModHelperData
+    {
+        public const string Name = "CashIDK";
+        public const string Description = "Mod mit Geld-Multiplikator, One-Hit-Kill, Godmode etc.";
+        public const string Version = "1.0.1";
+        public const string RepoOwner = "arnonymer66";
+        public const string RepoName = "CashIDK";
+    }
 }
