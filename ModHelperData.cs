89ea72a10ddbe4b4ff50f73fd13594374f88ffa0
arnonymer66 <arnonymer649@gmail.com>
2025-09-13 18:08:03 +0200

    Update ModHelperData.cs

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -1,11 +1,17 @@
-namespace CashIDK
+﻿namespace CashIDK;
+
+public static class ModHelperData
 {
-    public static class ModHelperData
-    {
-        public const string Name = "CashIDK";
-        public const string Description = "Mod mit Geld-Multiplikator, One-Hit-Kill, Godmode etc.";
-        public const string Version = "1.0.1";
-        public const string RepoOwner = "arnonymer66";
-        public const string RepoName = "CashIDK";
-    }
+    public const string WorksOnVersion = "35.0";
+    public const string Version = "1.0.1";
+    public const string Name = "CashIDK";
+
+    public const string Description = "Allows you to Multiply the Cash gained from ANY Source<br><br>" +
+        "F10: Set the Global Cash Multiplier<br><br>" +
+        "The global Multiplier counts to any allowed Cash-Source<br><br>" +
+        "The Options allow you to tweak which sources are allowed and how much they are multiplied<br>" +
+        "Allows decimals like 1.5 or 0.5 if you want to make things harder<br>";
+
+    public const string RepoOwner = "arnonymer66"; 
+    public const string RepoName = "CashIDK";
 }
