7f3b42db2e0ee2f836a5da25c662a685cd7a3079
arnonymer66 <arnonymer649@gmail.com>
2025-09-13 16:36:41 +0200

    Update ModHelperData.cs

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -2,12 +2,9 @@ namespace CashIDK;
 
 public static class ModHelperData
 {
-    public const string WorksOnVersion = "50.0";
-    public const string Version = "1.0.0";
     public const string Name = "CashIDK";
-
-    public const string Description = "An empty mod";
-
-    public const string RepoOwner = "arnonymer66"; // TODO add your github username hero, also in the download url in README.md
-    public const string RepoName = "CashIDK"; // TODO add your repo name here, also in the download url in README.md
+    public const string Description = "Mod mit Geld-Multiplikator, One-Hit-Kill, Godmode etc.";
+    public const string Version = "1.0.0";
+    public const string RepoOwner = "arnonymer66"; // <--- Hier kannst du deinen Namen eintragen
+    public const string RepoName = "CashIDK";
 }
