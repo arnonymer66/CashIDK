97c16bd830921b3ec79051fab4a38bb3cff4f6d3
arnonymer66 <arnonymer649@gmail.com>
2025-09-13 20:57:43 +0200

    Update ModHelperData.cs

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -3,7 +3,7 @@ namespace CashIDK;
 public static class ModHelperData
 {
     public const string WorksOnVersion = "50.0";
-    public const string Version = "1.0.3";
+    public const string Version = "1.0.4";
     public const string Name = "CashIDK";
 
     public const string RepoOwner = "arnonymer66"; 
