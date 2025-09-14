a8670e378d9907f91f810452d1ad34a4ab62ca21
arnonymer66 <arnonymer649@gmail.com>
2025-09-14 13:32:29 +0200

    Update ModHelperData.cs

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -3,7 +3,7 @@ namespace CashIDK;
 public static class ModHelperData
 {
     public const string WorksOnVersion = "50.0";
-    public const string Version = "1.1.1";
+    public const string Version = "1.1.2";
     public const string Name = "CashIDK";
 
     public const string Description = "Press (E) to Open/Close the Menu, Than type any number on the Keyboard (IMOPRTANT: The Numbers wront show up in the menu and you need to spam every number on you're Keyboard from 0-9 to gain any money in game and than enter, when you spam more numbers than you get more money and when you spam not that much numbers you dont get that much money)";
