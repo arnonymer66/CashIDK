72ed15a1cfcfa24069df2a94f87e679305da9d25
arnonymer66 <arnonymer649@gmail.com>
2025-09-14 11:41:44 +0200

    Update ModHelperData.cs

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -3,7 +3,7 @@ namespace CashIDK;
 public static class ModHelperData
 {
     public const string WorksOnVersion = "50.0";
-    public const string Version = "1.0.9";
+    public const string Version = "1.1.0";
     public const string Name = "CashIDK";
 
     public const string Description = "Go into the Settings to Open The menu";
