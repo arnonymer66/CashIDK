ac0fe23ab3d7d8ee0415d13006db6be37b174a56
arnonymer66 <arnonymer649@gmail.com>
2025-09-14 10:41:46 +0200

    Update CashIDK.cs

--- CashIDK.cs
+++ CashIDK.cs
@@ -9,6 +9,19 @@ using UnityEngine;
 [assembly: MelonGame("Ninja Kiwi", "BloonsTD6")]
 
 namespace MoneyInputMod
+{using MelonLoader;
+using BTD_Mod_Helper;
+using BTD_Mod_Helper.Api.ModOptions;
+using Il2CppAssets.Scripts.Unity.UI_New.InGame;
+using Il2CppAssets.Scripts.Unity.UI_New.MainMenu;
+using Il2CppAssets.Scripts.Simulation;
+using UnityEngine;
+using Il2CppAssets.Scripts.Simulation.Simulation;
+
+[assembly: MelonInfo(typeof(MoneyInputMod.Main), "Money Input Mod", "1.0.0", "DeinName")]
+[assembly: MelonGame("Ninja Kiwi", "BloonsTD6")]
+
+namespace MoneyInputMod
 {
     public class Main : BloonsTD6Mod
     {
@@ -25,20 +38,44 @@ namespace MoneyInputMod
 
         // Menü-Position und Größe
         private Rect menuRect = new Rect(10, 10, 250, 700);
-        private bool showGodModeSection = true;
-        private bool showMonkeyMoneySection = true;
-        private bool showMonkeyXPSection = true;
-        private bool showTowerSpeedSection = true;
-        private bool showTrophiesSection = true;
-        private bool showTowerDamageSection = true;
-        private bool showGameSpeedSection = true;
+        
+        // Referenzen zu den verschiedenen UI-Menüs
+        private GameObject modMenuUI = null;
+        private static ModSettings menuSettings;
 
-        public override void OnUpdate()
+        public override void OnSettingsUI()
         {
-            if (Input.GetKeyDown(KeyCode.E)) // "E" zum Ein-/Ausblenden des Menüs
+            // Hauptmenü-Button - wird im Hauptmenü angezeigt
+            if (GUILayout.Button("Mod Menü öffnen"))
             {
                 showMenu = !showMenu;
             }
+
+            base.OnSettingsUI();
+        }
+
+        public override void OnUpdate()
+        {
+            // Menüsteuerung
+            if (showMenu)
+            {
+                UpdateMenuOptions();
+            }
+        }
+
+        private void UpdateMenuOptions()
+        {
+            // Wenn der GodMode aktiviert ist, setze unendliches Leben für alle Türme
+            if (godModeEnabled)
+            {
+                foreach (var tower in InGame.instance.bridge.GetAllTowers())
+                {
+                    tower.life = 9999; // Maximale Leben
+                }
+            }
+
+            // Spielgeschwindigkeit einstellen
+            Time.timeScale = gameSpeed;
         }
 
         public override void OnGUI()
@@ -51,121 +88,77 @@ namespace MoneyInputMod
             GUILayout.Label("Monkey Mod Menu", GUILayout.Height(30));
 
             // God Mode - eigener Bereich
-            if (showGodModeSection)
-            {
-                GUILayout.BeginVertical("box");
-                GUILayout.Label("God Mode");
-                godModeEnabled = GUILayout.Toggle(godModeEnabled, "Aktivieren: " + (godModeEnabled ? "ON" : "OFF"));
-                if (godModeEnabled)
-                {
-                    foreach (var tower in InGame.instance.bridge.GetAllTowers())
-                    {
-                        tower.life = 9999; // Unendlich Leben für Türme
-                    }
-                }
-                GUILayout.EndVertical();
-            }
+            GUILayout.BeginVertical("box");
+            GUILayout.Label("God Mode");
+            godModeEnabled = GUILayout.Toggle(godModeEnabled, "Aktivieren: " + (godModeEnabled ? "ON" : "OFF"));
+            GUILayout.EndVertical();
 
             // Monkey Money - eigener Bereich
-            if (showMonkeyMoneySection)
+            GUILayout.BeginVertical("box");
+            GUILayout.Label("Monkey Money");
+            monkeyMoney = (int)GUILayout.HorizontalSlider(monkeyMoney, 0, 10000);
+            GUILayout.Label("Wert: " + monkeyMoney);
+            if (GUILayout.Button("Add Monkey Money"))
             {
-                GUILayout.BeginVertical("box");
-                GUILayout.Label("Monkey Money");
-                monkeyMoney = (int)GUILayout.HorizontalSlider(monkeyMoney, 0, 10000);
-                GUILayout.Label("Wert: " + monkeyMoney);
-                if (GUILayout.Button("Add Monkey Money"))
+                if (InGame.instance != null && InGame.instance.bridge != null)
                 {
-                    if (InGame.instance != null && InGame.instance.bridge != null)
-                    {
-                        InGame.instance.bridge.AddCash(monkeyMoney, (Il2CppAssets.Scripts.Simulation.Simulation.CashSource)3);
-                        MelonLogger.Msg($"Spieler bekam {monkeyMoney} Monkey Money.");
-                    }
+                    InGame.instance.bridge.AddCash(monkeyMoney, (CashSource)3);
+                    MelonLogger.Msg($"Spieler bekam {monkeyMoney} Monkey Money.");
                 }
-                GUILayout.EndVertical();
             }
+            GUILayout.EndVertical();
 
-            // Affen XP - eigener Bereich
-            if (showMonkeyXPSection)
+            // Monkey XP - eigener Bereich
+            GUILayout.BeginVertical("box");
+            GUILayout.Label("Affen XP");
+            monkeyXP = (int)GUILayout.HorizontalSlider(monkeyXP, 0, 10000);
+            GUILayout.Label("Wert: " + monkeyXP);
+            if (GUILayout.Button("Add Monkey XP"))
             {
-                GUILayout.BeginVertical("box");
-                GUILayout.Label("Affen XP");
-                monkeyXP = (int)GUILayout.HorizontalSlider(monkeyXP, 0, 10000);
-                GUILayout.Label("Wert: " + monkeyXP);
-                if (GUILayout.Button("Add Monkey XP"))
+                var player = InGame.instance.bridge.GetPlayer();
+                if (player != null)
                 {
-                    var player = InGame.instance.bridge.GetPlayer();
                     player.AddXP(monkeyXP);
                     MelonLogger.Msg($"Spieler bekam {monkeyXP} XP.");
                 }
-                GUILayout.EndVertical();
             }
-
-            // Türmenschussgeschwindigkeit - eigener Bereich
-            if (showTowerSpeedSection)
-            {
-                GUILayout.BeginVertical("box");
-                GUILayout.Label("Türmenschussgeschwindigkeit");
-                towerSpeed = GUILayout.HorizontalSlider(towerSpeed, 0.1f, 5.0f);
-                GUILayout.Label("Wert: " + towerSpeed.ToString("F2"));
-                if (GUILayout.Button("Set Tower Speed"))
-                {
-                    foreach (var tower in InGame.instance.bridge.GetAllTowers())
-                    {
-                        tower.projectileSpeed = towerSpeed;
-                    }
-                    MelonLogger.Msg($"Schussgeschwindigkeit der Türme auf {towerSpeed} gesetzt.");
-                }
-                GUILayout.EndVertical();
-            }
-
-            // Trophäen - eigener Bereich
-            if (showTrophiesSection)
-            {
-                GUILayout.BeginVertical("box");
-                GUILayout.Label("Trophäen: " + trophies);
-                if (GUILayout.Button("Add Trophy"))
-                {
-                    trophies++;
-                    MelonLogger.Msg($"Trophäen erhöht: {trophies}");
-                }
-                GUILayout.EndVertical();
-            }
-
-            // Affenschaden - eigener Bereich
-            if (showTowerDamageSection)
-            {
-                GUILayout.BeginVertical("box");
-                GUILayout.Label("Affenschaden Multiplier");
-                towerDamageMultiplier = GUILayout.HorizontalSlider(towerDamageMultiplier, 0.1f, 10.0f);
-                GUILayout.Label("Wert: " + towerDamageMultiplier.ToString("F2"));
-                if (GUILayout.Button("Set Tower Damage"))
-                {
-                    foreach (var tower in InGame.instance.bridge.GetAllTowers())
-                    {
-                        // Beispiel: Erhöhe den Schaden des Turms
-                        tower.attack *= towerDamageMultiplier;
-                    }
-                    MelonLogger.Msg($"Affenschaden der Türme auf {towerDamageMultiplier} gesetzt.");
-                }
-                GUILayout.EndVertical();
-            }
+            GUILayout.EndVertical();
 
             // Spielgeschwindigkeit - eigener Bereich
-            if (showGameSpeedSection)
-            {
-                GUILayout.BeginVertical("box");
-                GUILayout.Label("Spielgeschwindigkeit");
-                gameSpeed = GUILayout.HorizontalSlider(gameSpeed, 0.1f, 5.0f);
-                GUILayout.Label("Wert: " + gameSpeed.ToString("F2"));
-                if (GUILayout.Button("Set Game Speed"))
-                {
-                    Time.timeScale = gameSpeed;
-                    MelonLogger.Msg($"Spielgeschwindigkeit auf {gameSpeed} gesetzt.");
-                }
-                GUILayout.EndVertical();
-            }
+            GUILayout.BeginVertical("box");
+            GUILayout.Label("Spielgeschwindigkeit");
+            gameSpeed = GUILayout.HorizontalSlider(gameSpeed, 0.1f, 5.0f);
+            GUILayout.Label("Wert: " + gameSpeed.ToString("F2"));
+            GUILayout.EndVertical();
+
+            // Affenschaden - eigener Bereich
+            GUILayout.BeginVertical("box");
+            GUILayout.Label("Affenschaden Multiplier");
+            towerDamageMultiplier = GUILayout.HorizontalSlider(towerDamageMultiplier, 0.1f, 10.0f);
+            GUILayout.Label("Wert: " + towerDamageMultiplier.ToString("F2"));
+            GUILayout.EndVertical();
 
             GUILayout.EndArea();
+        }
+
+        // Mod-Menü im Startmenü anzeigen
+        public override void OnMainMenuUI()
+        {
+            // Button im Profilbereich oder Einstellungen hinzufügen
+            if (GUILayout.Button("Mod Menü öffnen"))
+            {
+                showMenu = !showMenu;
+            }
+        }
+
+        // Pause-Menü-Modifikation
+        public override void OnInGameUI()
+        {
+            // Button im Pause-Menü hinzufügen
+            if (GUILayout.Button("Mod Menü öffnen"))
+            {
+                showMenu = !showMenu;
+            }
         }
     }
 }
