597e3c26126e21d66258cd65b022249d9e10bef3
arnonymer66 <arnonymer649@gmail.com>
2025-09-14 11:23:04 +0200

    Update ModHelperData.cs

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -3,10 +3,10 @@ namespace CashIDK;
 public static class ModHelperData
 {
     public const string WorksOnVersion = "50.0";
-    public const string Version = "1.0.8";
+    public const string Version = "1.0.9";
     public const string Name = "CashIDK";
 
-    public const string Description = "Press (E) to Open The menu, than type any amount of money you want in game, than press Enter to Reedem it. Works only in game cash Not Monkey Dollar";
+    public const string Description = "Go into the Settings to Open The menu";
     
     public const string RepoOwner = "arnonymer66"; 
     public const string RepoName = "CashIDK";
