86af172017c9f16ecd5ddbbd6243d9c6c46017dc
arnonymer66 <arnonymer649@gmail.com>
2025-09-16 14:10:19 +0200

    Update ModHelperData.cs

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -3,10 +3,10 @@ namespace CashIDK;
 public static class ModHelperData
 {
     public const string WorksOnVersion = "50.0";
-    public const string Version = "1.1.3";
+    public const string Version = "1.1.4";
     public const string Name = "CashIDK";
 
-    public const string Description = "Press (E) to Open/Close the Menu, Than type any number on the Keyboard (IMOPRTANT: The Numbers wront show up in the menu and you need to spam every number on you're Keyboard from 0-9 to gain any money in game and than enter, when you spam more numbers than you get more money and when you spam not that much numbers you dont get that much money)";
+    public const string Description = "Press (E) to Open/Close the Menu, Than type any number on the Keyboard (IMOPRTANT: The Numbers wront show up in the menu and you need to spam every number on you're Keyboard from 0-9 to gain any money in game and than enter, when you spam more numbers than you get more money and when you spam not that much numbers you dont get that much money,IDK this is buggy)";
     
     public const string RepoOwner = "arnonymer66"; 
     public const string RepoName = "CashIDK";
